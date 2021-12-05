eb6a1973974f1380d5b8ae01c4d5a775538567f4
mohamad-azizzadeh81 <94900086+mohamad-azizzadeh81@users.noreply.github.com>
2021-12-05 17:20:27 +0330

    btn
    Support

---
+++ indax.js
@@ -0,0 +1,9 @@
+function openchatbtn(){
+    document.getElementById("chat").style.display="block"
+}
+function closechatbtn(){
+    document.getElementById("chat").style.display="none"
+}
+function operWindow(){
+    window.open("https://www.gmail.com")
+}
